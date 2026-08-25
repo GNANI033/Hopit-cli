292f08dd689bee601d82bbbdb5247dc4946b1dd8
GNANI033 <gnanaeshwarramesh@yahoo.com>
2026-08-25 22:29:22 +0530

    windows bug fix

--- HopitLauncher.cs
+++ HopitLauncher.cs
@@ -16,11 +16,13 @@ namespace HopitLauncher
             string pythonPath = Path.Combine(baseDir, "python", "python.exe");
             string scriptPath = Path.Combine(baseDir, "src", "hopit-cli.py");
 
+            string srcDir = Path.Combine(baseDir, "src");
+
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
                 FileName = "cmd.exe",
-                // /c runs the command and then we explicitly call 'pause' so the window stays open forever
-                Arguments = $"/c \" \"{pythonPath}\" \"{scriptPath}\" & echo. & echo Program exited. & pause \"",
+                // UAC ignores WorkingDirectory, so we force cmd to 'cd /d' to the src folder first!
+                Arguments = "/c \"cd /d \"" + srcDir + "\" && \"" + pythonPath + "\" \"" + scriptPath + "\" & echo. & echo Program exited. & pause \"",
                 UseShellExecute = true,
                 Verb = "runas"
             };
